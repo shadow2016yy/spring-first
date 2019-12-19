7ebceb4e0990859ee39cec7356cf03d0b9fad757
chenluming <shadow_chen_sh@163.com>
2019-12-19 10:51:10 +0800

    complete groovy handle mysql

--- src/main/java/com/ryan/www/groovy/DBTest.groovy
+++ src/main/java/com/ryan/www/groovy/DBTest.groovy
@@ -1,7 +1,21 @@
 package com.ryan.www.groovy
 
+import groovy.sql.Sql
+
 /**
  * Created by Ryan on 2018/11/10.
  */
 class DBTest {
+    static void main(String[] args) {
+        def db_url = "jdbc:mysql://localhost:3306/test"
+        def username = "root"
+        def password = "root"
+        def driverClass = "com.mysql.jdbc.Driver"
+        def sql = Sql.newInstance(db_url, username, password, driverClass)
+        sql.eachRow(" select * from td_hdk_region "){
+            row-> println(row)
+        }
+
+
+    }
 }
